97fe3d26ca4068aa6fd4f5df2e6d471309b61611
Ashley Orelien <ashleyorelien@Ashleys-MacBook-Air.local>
2022-02-01 08:58:23 -0500

    Version 2

---
+++ src/main/java/lab18/ItemsEnum.java
@@ -0,0 +1,5 @@
+package lab18;
+
+public enum ItemsEnum {
+    WATER, SODA, CHIPS, CHOCOLATE, POPCORN
+}
--- src/test/java/lab18/TerminalTest.java
+++
@@ -1,8 +0,0 @@
-package lab18;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class TerminalTest {}
-
-
